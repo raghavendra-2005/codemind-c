9657169fd28765491116fb01c1024fc86d35461f
Ballamudi Raghavendra <147475199+raghavendra-2005@users.noreply.github.com>
2023-10-14 20:52:32 +0530

    #include <stdio.h> int main() {     int x;     scanf("%d",&x);    if(x<96)    {        printf("NO");    }     else if(x<100)     {         printf("NO");     }     else if(x=96)     {         printf("YES");     }     else if(x>102)     {         printf("YES");     } }

---
+++ Fever.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+   if(x<96)
+   {
+       printf("NO");
+   }
+    else if(x<100)
+    {
+        printf("NO");
+    }
+    else if(x=96)
+    {
+        printf("YES");
+    }
+    else if(x>102)
+    {
+        printf("YES");
+    }
+}
